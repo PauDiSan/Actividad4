37a302ef3ebe38784c718c4276aa1b8c8cf29a74
ivaandiaz <ivan.diazfernandez@iesmiguelherrero.com>
2022-12-16 19:29:25 +0100

    cambiado jose

--- ejercicio4/src/main/java/entornosdesarrollo/ejercicio4/Ejercicio4.java
+++ ejercicio4/src/main/java/entornosdesarrollo/ejercicio4/Ejercicio4.java
@@ -12,6 +12,6 @@ package entornosdesarrollo.ejercicio4;
 public class Ejercicio4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Paula");
+        System.out.println("Hello Jose");
     }
 }
